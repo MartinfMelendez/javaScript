0c28d11b7b28d17d4a33f190670ba29f8b385c7a
MartinfMelendez <martinf.melendez@gmail.com>
2024-12-05 22:47:03 -0300

    Se agregar comentarios

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ El numero a adivinar se elige de forma aleatorea
 Una vez que el jugador adivine el numero o se quede sin intentos el sistema le preguntara si desea volver a jugar
 Se debera crear una funcion para validar si el numero ingresado por el usuario es valido y esta dentro del rango 0 a 10*/
 
-const validarNumero = (num) => {
+const validarNumero = (num) => { // Funcion para validar el numero ingresado
   if (isNaN(num) || num < 0 || num > 10) {
     alert(
       "El numero ingresado no es valdio o se encuentra fuera del rango 0 al 10"
@@ -20,23 +20,23 @@ let continuar = true;
 let cpu = Math.floor(Math.random() * 11);
 let intentos = 3;
 
-const reiniciar = () => {
+const reiniciar = () => { //Funcion para reiniciar el juego
   intentos = 3;
   cpu = Math.floor(Math.random() * 11);
 };
 
-function adivinarNumero() {
+function adivinarNumero() { 
   console.log(cpu);
   console.log(intentos);
   do {
     const numUser = parseInt(prompt("Ingrese un numero entre 0 y 10"));
-    if (!validarNumero(numUser)) {
+    if (!validarNumero(numUser)) { //Se llama a la funcion validar numero para ver si el numero que se ingreso es valido
       continue;
     }
 
-    if (cpu === numUser) {
+    if (cpu === numUser) { //Es la condicion de victoria
       alert("Felicitaciones!. Adivinaste el numero");
-      if (confirm("Desea jugar de nuevo")) {
+      if (confirm("Desea jugar de nuevo")) { //Se usa un confirm para saber si el jugador desea volver a jugar
         reiniciar();
       } else {
         break;
