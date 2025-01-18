8b4fdb9f1f9c4333c71d62827269cdc3e4d20f47
Martin Melendez <martinf.melendez@gmail.com>
2025-01-18 19:57:55 -0300

    se crea funcion para crear las card y no repetir codigo

--- GaiaFit/index.js
+++ GaiaFit/index.js
@@ -2,35 +2,24 @@
 //Funcion para cargar todos los productos del localStorage cuando se inicializa la pagina
 const preload = () => {
     let productos = JSON.parse(localStorage.getItem('productos'))
-    let container = document.querySelector('.container-card')
-    //let contenidoHTML =''
 
     if (productos) {
         productos.forEach(x => {
-            let card = document.createElement('div');
-            card.classList.add('card')
-            card.innerHTML += `<img src="${x.imagen}">
-        <p>Nombre: <b>${x.nombre}</b></p>
-          <p>Precio: ${x.precio}</p>
-          <p>Stock: ${x.stock}</p>
-          <button>Comprar</button>`
-
-            container.appendChild(card)
+            crearCard(x.imagen, x.nombre, x.precio, x.stock)
         })
     }
 }
 
-// const crearCard = (img, nombre, precio, stock) => {
-//     let contenedor = document.querySelector('.container-card')
-//     let card = document.createElement('div')
-//     card.classList.add('card')
-//     card.innerHTML += ` 
-//     <img src='${img}'></img> <h4>Nombre: ${nombre}</h4>
-//   <p>Precio:$${precio}</p>
-//   <p>Stock: ${stock}</p>
-//   <button>Comprar</button>`
-  
-//     contenedor.appendChild(card)
-//   }
+const crearCard = (img, nombre, precio, stock) => {
+    let contenedor = document.querySelector('.container-card')
+    let card = document.createElement('div')
+    card.classList.add('card')
+    card.innerHTML += `<img src="${img}">
+    <p>Nombre: <b>${nombre}</b></p>
+      <p>Precio: ${precio}</p>
+      <p>Stock: ${stock}</p>
+      <button value='agregar'>Comprar</button>`
 
+    contenedor.appendChild(card)
+}
 preload()
